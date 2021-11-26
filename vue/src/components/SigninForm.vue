155ba07076d58a88aaf3bf4b6e4b59cb714abc47
yumekiti <yumekiti120440@gmail.com>
2021-11-26 11:59:09 +0900

    add: SigninForm

--- vue/src/components/SigninForm.vue
+++ vue/src/components/SigninForm.vue
@@ -1,35 +1,56 @@
 <template>
     <div>
-        <v-container cols="12">
-            <v-card>
-                <v-row class="my-auto">
-                    <v-col cols="10" class="mx-auto">
-                        <v-text-field
-                            label="email"
-                            clearable
-                        ></v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row class="my-auto">
-                    <v-col cols="10" class="mx-auto">
-                        <v-text-field
-                            label="password"
-                            clearable
-                        ></v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="8" />
-                    <v-col cols="3">
-                        <v-btn>Signin</v-btn>
-                    </v-col>
-                </v-row>
-            </v-card>
+        <v-container style="display: flex; margin-top: 130px">
+            <v-row style="align-items: center; justify-content: center;">
+                <v-col cols="12" md="6">
+
+                    <v-card>
+                        <v-card-title>
+                            <h1 class="display-1">Signin</h1>
+                        </v-card-title>
+                        
+                        <v-card-text>
+                            <v-form>
+
+                                <v-text-field
+                                    prepend-icon="mdi-account-circle"
+                                    label="Email"
+                                />
+
+                                <v-text-field
+                                    v-bind:type="showPassword ? 'text' : 'password'"
+                                    @click:append="showPassword = !showPassword"
+                                    prepend-icon="mdi-lock" 
+                                    v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                    label="Password"
+                                />
+
+                                <v-card-actions>
+                                    <v-card to="/Signup">
+                                        <v-btn style="font-size: 10px;">
+                                            Signup
+                                        </v-btn>
+                                    </v-card>
+                                    <v-spacer></v-spacer>
+                                    <v-btn class="info">Signup</v-btn>
+                                </v-card-actions>
+
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
 <script>
 export default {
     name: 'SigninForm',
+    data: () => {
+        return {
+            showPassword: false,
+        }
+    },
 }
 </script>
