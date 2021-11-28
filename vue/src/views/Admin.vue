c05128f2c436346926c77e170eb4948394c40086
yumekiti <yumekiti120440@gmail.com>
2021-11-29 04:32:51 +0900

    change: admin nemukunatayo---

--- vue/src/views/Admin.vue
+++ vue/src/views/Admin.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-        {{this.user}}
+        <Header :title="'admin'" />
+        <v-container>
+            <v-row cols="12">
+                <v-col cols="12">
+                    <router-link :to="'/admin/user'">
+                        <v-btn block color="primary">
+                            UserList
+                        </v-btn>
+                    </router-link>
+                </v-col>
+                <v-col cols="12">
+                    <router-link :to="'/admin/stamp'">
+                        <v-btn block color="primary">
+                            StampList
+                        </v-btn>
+                    </router-link>
+                </v-col>
+                <v-col cols="12">
+                    <router-link :to="'/admin/achievement'">
+                        <v-btn block color="primary">
+                            AchievementList
+                        </v-btn>
+                    </router-link>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 <script>
+import Header from '@/components/Header.vue'
+
 export default {
     name: 'Admin',
+    components: {
+        Header,
+    },
     data: () => {
         return {
             user: null
