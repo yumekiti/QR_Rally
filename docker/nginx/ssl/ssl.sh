0d43a225ed93879860e2f02d69b0e976ccd304a6
yumekiti <yumekiti120440@gmail.com>
2021-11-30 18:12:11 +0900

    fix: nginx

--- docker/nginx/ssl/ssl.sh
+++ docker/nginx/ssl/ssl.sh
@@ -3,6 +3,7 @@ if (cat ./docker/docker-compose.yml | grep /nginx/conf.d > /dev/null 2>&1); then
     openssl req -x509 -nodes -new -keyout server.key -out server.crt -days 365 && \
     cd ../../ && \
     sed -i -e "s/image: nginx:alpine/build: .\/nginx\/ssl\//g" docker-compose.yml ; \
+    sed -i -e "10d" docker-compose.yml ; \
     sed -i -e "11d" docker-compose.yml ; \
     cd ../
 else
