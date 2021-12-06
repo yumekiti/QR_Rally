495f4d3380cde289b164c9f29291893c94d2896e
yumekiti <yumekiti120440@gmail.com>
2021-12-06 21:53:02 +0900

    change: rule

--- vue/src/components/Rule.vue
+++ vue/src/components/Rule.vue
@@ -1,17 +1,7 @@
 <template>
-    <div class="mt-3">
-        <v-container>
-            <v-row>
-                <v-col cols="12">
-                    <strong>{{this.title}}</strong>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="12">
-                    {{this.detail}}
-                </v-col>
-            </v-row>
-        </v-container>
+    <div class="main__rule">
+        <h2 style="white-space: pre-wrap;" v-text="this.title"></h2>
+        <p style="white-space: pre-wrap;" v-text="this.detail"></p>
     </div>
 </template>
 <script>
@@ -23,3 +13,23 @@ export default {
     }
 }
 </script>
+<style scoped>
+/* ルール説明部分 */
+.main__rule {
+    text-align: center;
+}
+
+.main__rule h2 {
+    font-size: 1.6rem;
+    margin: 3.2rem 0 0.8rem 0;
+    color: #25a5ec;
+}
+
+.main__rule p {
+    font-size: 1.2rem;
+    padding: 1.6rem;
+    letter-spacing: 0.1rem;
+    color: #fff;
+    background: #25a5ec;
+}
+</style>
